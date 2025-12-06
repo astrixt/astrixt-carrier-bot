cb619276bced0cf6d8d5a1336a70c5ef66a26513
astrixt <astrix.t2025@gmail.com>
2025-12-06 15:01:31 +0100

    Update configuration for Telegram bot and Google Sheets

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-# === TELEGRAM BOT ===
+# config.py
+
 BOT_TOKEN = "7985166141:AAEIEZBvuL-bBHdouSdzHkcHj5PJSW6Y3Iw"
 
-# === GOOGLE SHEETS ===
-SPREADSHEET_ID = "1X-JFBLG_vJ6OjfaO-hjfQmwSdpouvSnLoG9ydo1Zyis"   # без /edit в конце
-SHEET_NAME = "Лист1"  # или другое название, если менял
+# Google Sheet для запису результатів опитування
+SPREADSHEET_ID = "1X-JFBLG_vJ6OJfa0-hfjQmwSdpouvSnLoG9ydo1Zys"
 
-# === ДАННЫЕ КАНАЛА ===
-CHANNEL_URL = "https://t.me/@astrixt2022"
+# Назва листа (на скріні “Лист1”)
+SHEET_NAME = "Лист1"
